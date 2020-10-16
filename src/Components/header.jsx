b3a01bdbfee5547b14026e83def0bb73c3f5e037
unknown <ghareeb_shehata@outlook.com>
2020-10-16 16:29:38 +0200

    First commit : Routing & Switch

---
+++ src/Components/header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <header>
+            <nav>
+                <div className="logo">
+                    <h1>Logo</h1>
+                </div>
+                <ul>
+                    <li><Link className="links" to="/">Home</Link></li>
+                    <li><Link className="links" to="/about">About</Link></li>
+                    <li><Link className="links" to="/contact">Contact Us</Link></li>
+                </ul>
+
+            </nav>
+        </header>
+    )
+}
+
+export default Nav;
