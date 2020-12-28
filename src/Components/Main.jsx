51989de76c8b20bbae5cc0a3bf3ce20f1292cb88
unknown <ghareeb_shehata@outlook.com>
2020-12-28 07:02:16 +0200

    Setting the components of the website after building the structure on paper

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import main from './Main.module.scss';
 
 
 const Main = () => {
@@ -6,9 +7,36 @@ const Main = () => {
 
     const [city, setCity] = useState("");
     const [weather, setWeather] = useState({});
-
+    const [coords, setCoords] = useState({});
     const handleChange = (e) => {
         setCity(e.target.value);
+    }
+
+    useEffect(() => {
+        if (window.navigator.geolocation) {
+            window.navigator.geolocation.getCurrentPosition(position => {
+                setCoords({
+                    lat: position.coords.latitude,
+                    long: position.coords.longitude
+                })
+            });
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+
+    }, [])
+
+    const getLocation = () => {
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.long}&units=metric&lang=ar&appid=${apiKey}`)
+            // fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.long}&units=metric&lang=ar&appid=${apiKey}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
+    const getLocation2 = () => {
+        // fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.long}&units=metric&lang=ar&appid=${apiKey}`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.long}&units=metric&lang=ar&appid=${apiKey}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
     }
 
     const get_weather = (city) => {
@@ -17,6 +45,10 @@ const Main = () => {
             .then(data => console.log(data))
             .then(setCity(""))
     }
+    const unix = 1609115724;
+    const ms = unix * 1000;
+    const date = new Date(ms);
+    console.log(date.toLocaleString());
 
     return (
         <div className="weather">
@@ -30,6 +62,9 @@ const Main = () => {
                     get_weather(city)
                 }}>Get Weather</button>
             </form>
+            <button onClick={getLocation}>Get Location</button>
+            <button onClick={getLocation2}>Get Location</button>
+
         </div>
     )
 }
---
+++ src/imp.js
@@ -0,0 +1,30 @@
+const curunt_weather = {
+    name: `api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}`,
+    coords: `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`,
+
+}
+
+dateObject.toLocaleString("en-US", {
+    weekday: "long"
+}) // Monday
+dateObject.toLocaleString("en-US", {
+    month: "long"
+}) // December
+dateObject.toLocaleString("en-US", {
+    day: "numeric"
+}) // 9
+dateObject.toLocaleString("en-US", {
+    year: "numeric"
+}) // 2019
+dateObject.toLocaleString("en-US", {
+    hour: "numeric"
+}) // 10 AM
+dateObject.toLocaleString("en-US", {
+    minute: "numeric"
+}) // 30
+dateObject.toLocaleString("en-US", {
+    second: "numeric"
+}) // 15
+dateObject.toLocaleString("en-US", {
+    timeZoneName: "short"
+}) // 12/9/2019, 10:30:15 AM CST
