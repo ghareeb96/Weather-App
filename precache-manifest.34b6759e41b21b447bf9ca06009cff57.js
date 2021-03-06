670778e33081a1a7a34e43cedbf170da583f1b08
unknown <ghareeb_shehata@outlook.com>
2021-03-06 08:20:36 +0200

    Updates

---
+++ precache-manifest.34b6759e41b21b447bf9ca06009cff57.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4980cadd5bf34165d76f7351011098a",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "2e443a2f86130b8b536c",
+    "url": "/Weather-App/static/css/main.bd8c03f5.chunk.css"
+  },
+  {
+    "revision": "11356fbf033923ee79c8",
+    "url": "/Weather-App/static/js/2.96514bb0.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/Weather-App/static/js/2.96514bb0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e443a2f86130b8b536c",
+    "url": "/Weather-App/static/js/main.a452618c.chunk.js"
+  },
+  {
+    "revision": "f7a0b8093de90b86c245",
+    "url": "/Weather-App/static/js/runtime-main.2f2b1e57.js"
+  },
+  {
+    "revision": "60ce5114b0b233a254a42e7a510b702e",
+    "url": "/Weather-App/static/media/Palestine.60ce5114.jpg"
+  },
+  {
+    "revision": "f7efb53c883ee9ee4af05bc1dee8daef",
+    "url": "/Weather-App/static/media/humidity.f7efb53c.svg"
+  },
+  {
+    "revision": "d9d208a8fdf6b1d9faf33baae414944f",
+    "url": "/Weather-App/static/media/maps-and-flags.d9d208a8.svg"
+  },
+  {
+    "revision": "f731ab6ab14e307d87a1e613375f843a",
+    "url": "/Weather-App/static/media/pressure.f731ab6a.svg"
+  },
+  {
+    "revision": "5f95999bb151d0063752c4c8867e20ef",
+    "url": "/Weather-App/static/media/rain.5f95999b.svg"
+  },
+  {
+    "revision": "f62d4aa0edcad94635ed950a5378c63e",
+    "url": "/Weather-App/static/media/search-interface-symbol.f62d4aa0.svg"
+  },
+  {
+    "revision": "4b4cf493f88fb594bc1a7072e5b52902",
+    "url": "/Weather-App/static/media/visibility.4b4cf493.svg"
+  },
+  {
+    "revision": "9737c2a497c608ed2aa4f3f982d45f14",
+    "url": "/Weather-App/static/media/wind.9737c2a4.svg"
+  }
+]);
